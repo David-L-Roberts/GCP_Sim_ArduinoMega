95e9c073c6f2ad8a75729127d0293e75a81d915a
David-L-Roberts <robed13mail@gmail.com>
2025-02-16 23:47:24 +1030

    misc

--- include/ComsAPI.h
+++ include/ComsAPI.h
@@ -1,15 +1,11 @@
 #pragma once
 
-// action code for changing switching time. Lets system know to interpret the next message as a time value.
-#define CHANGE_SWITCH_T 200
 // received switching time is multiplied by this number (e.g. recv <250>; switching time becomes 250*4 = 1000 ms)
 #define SWITCH_T_MULT 4
 // minimum allowed swtching time (in ms)
 #define SWITCH_T_MIN 100
 
-#define HMI_ACK 253     // action code for acknowledging HMI hello
-#define HMI_HELLO 254   // action code for startup hello from HMI
-#define NO_CODE 255     // number used to signify when there is no current action code to execute
+// ===================================
 
 enum CycleMode {
     DECREASE_EZ     = 100,
@@ -19,3 +15,9 @@ enum CycleMode {
     MANUAL          = 110,
     IDLE            = 111
 };
+
+// action code for changing switching time. Lets system know to interpret the next message as a time value.
+#define CHANGE_SWITCH_T 200
+#define HMI_ACK         253     // action code for acknowledging HMI hello
+#define HMI_HELLO       254     // action code for startup hello from HMI
+#define NO_CODE         255     // number used to signify when there is no current action code to execute
