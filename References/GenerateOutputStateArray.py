94d4775002d05fbda17c279085b931afbbeacf5f
David-L-Roberts <robed13mail@gmail.com>
2025-02-20 22:33:17 +1030

    removed states that increase EZ

--- References/GenerateOutputStateArray.py
+++ References/GenerateOutputStateArray.py
@@ -1,8 +1,13 @@
 # Function to format and print a binary number
-def write_binary_strings_to_file(num_bits, count, file_name):
+def write_binary_strings_to_file(num_bits: int, count: int, file_name: str, remove_list: list):
     with open(file_name, "w") as file:
         state_num = 0
         for i in range(count):
+            
+            # apply removal list
+            if (i in remove_list):
+                continue
+
             # Generate the binary representation of the number
             binary_str = f"{i:0{num_bits}b}"  # Format the number as a binary string with leading zeros
 
@@ -18,7 +23,7 @@ def write_binary_strings_to_file(num_bits, count, file_name):
             formatted_str = "{" + ", ".join(binary_str) + "}"
 
             # Print the formatted string
-            file.write(f'{formatted_str}, // state {state_num} \n')
+            file.write(f'{formatted_str}, // state {state_num} -> GID {i} \n')
 
             state_num += 1
     
@@ -29,8 +34,18 @@ def write_binary_strings_to_file(num_bits, count, file_name):
 num_bits = 9            # Number of binary digits
 count = 2**num_bits     # Number of binary strings to generate (2^9)
 file_name = "OutputStateArray.txt"
+# list of states to manually remove. Using GID reference
+remove_list = [
+    64,  65,
+    128, 129,
+    192, 193,
+    256, 257,
+    320, 321,
+    384, 385,
+    448, 449
+]
 
 # Print the binary strings
-total_states = write_binary_strings_to_file(num_bits, count, file_name)
+total_states = write_binary_strings_to_file(num_bits, count, file_name, remove_list)
 
 print(f"Binary strings (count: {total_states}) written to: {file_name}")
